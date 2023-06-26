63b9c3aa3acaff1903a84092d4215f1d1b8ef235
magamanukyan <manukyanmaga98@gmail.com>
2023-06-26 21:20:20 +0400

    small

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const ProjectCard = ({index, name, description, tags, image, source_code_link, source_link})=>{
   return (
-    <motion.div onClick={()=>window.open( source_link, '_blank')} variants={fadeIn('up', 'spring', index*0.5, 0.75)}>
+    <motion.div  variants={fadeIn('up', 'spring', index*0.5, 0.75)}>
       <Tilt
         options={{
           max:45,
@@ -18,7 +18,7 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link, s
           speed:450,
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
-          <div className='relative w-full h-[230px]'>
+          <div onClick={()=>window.open( source_link, '_blank')} className='relative w-full h-[230px]'>
             <img 
               src={image}
               alt={name}
